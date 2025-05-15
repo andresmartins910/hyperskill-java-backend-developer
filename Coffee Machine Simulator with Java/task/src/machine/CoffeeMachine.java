f2d2163fd483d7406da3550f3fe40fdb85d91909
André Martins <andresmartins910@gmail.com>
2025-05-14 22:00:28 -0300

    coffee machine lesson 5 + update README.md

--- Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
+++ Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
@@ -9,77 +9,112 @@ public class CoffeeMachine {
     static int coffeeBeans = 120;
     static int disposableCups = 9;
     static int money = 550;
+    static boolean running = true;
 
     public static void main(String[] args) {
-        printState();
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Write action (buy, fill, take): ");
-            String action = scanner.next().toLowerCase();
+            while (running) {
+                System.out.print("Write action (buy, fill, take, remaining, exit): ");
+                String action = scanner.next().toLowerCase();
 
-            switch (action) {
-                case "buy"  -> handleBuy(scanner);
-                case "fill" -> handleFill(scanner);
-                case "take" -> handleTake();
-                default     -> System.out.println("Unknown command: " + action);
+                switch (action) {
+                    case "buy"       -> handleBuy(scanner);
+                    case "fill"      -> handleFill(scanner);
+                    case "take"      -> handleTake();
+                    case "remaining" -> printState();
+                    case "exit"      -> running = false;
+                    default          -> System.out.print("Unknown command: " + action);
+                }
             }
-            printState();
         }
     }
 
     public static void printState() {
-        System.out.println("The coffee machine has: ");
-        System.out.println(water + " ml of water");
-        System.out.println(milk + " ml of milk");
-        System.out.println(coffeeBeans + " g of coffee beans");
-        System.out.println(disposableCups + " disposable cups");
-        System.out.println("$" + money + " of money");
+        System.out.print("The coffee machine has: ");
+        System.out.print(water + " ml of water");
+        System.out.print(milk + " ml of milk");
+        System.out.print(coffeeBeans + " g of coffee beans");
+        System.out.print(disposableCups + " disposable cups");
+        System.out.print("$" + money + " of money");
     }
 
     public static void handleBuy(Scanner scanner) {
-        System.out.print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-        int coffeeType = scanner.nextInt();
+        System.out.print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
+        String choice = scanner.next().toLowerCase();
 
-        switch (coffeeType) {
-            case 1 -> {            // espresso
-                water -= 250;
-                coffeeBeans -= 16;
-                money += 4;
-                disposableCups--;
+        switch (choice) {
+            case "1" -> { // espresso
+                if (water >= 250 && coffeeBeans >= 16 && disposableCups >= 1) {
+                    water -= 250;
+                    coffeeBeans -= 16;
+                    money += 4;
+                    disposableCups--;
+                    System.out.print("I have enough resources, making you a coffee!");
+                } else if (water < 250) {
+                    System.out.print("Sorry, not enough water!");
+                } else if (coffeeBeans < 16) {
+                    System.out.print("Sorry, not enough coffee beans!");
+                } else if (disposableCups == 0) {
+                    System.out.print("Sorry, not enough disposable cups!");
+                }
             }
-            case 2 -> {            // latte
-                water -= 350;
-                milk -= 75;
-                coffeeBeans -= 20;
-                money += 7;
-                disposableCups--;
+            case "2" -> { // latte
+                if (water >= 350 && milk >= 75 && coffeeBeans >= 20 && disposableCups >= 1) {
+                    water -= 350;
+                    milk -= 75;
+                    coffeeBeans -= 20;
+                    money += 7;
+                    disposableCups--;
+                    System.out.print("I have enough resources, making you a coffee!");
+                } else if (water < 350) {
+                    System.out.print("Sorry, not enough water!");
+                } else if (milk < 75) {
+                    System.out.print("Sorry, not enough milk!");
+                } else if (coffeeBeans < 20) {
+                    System.out.print("Sorry, not enough coffee beans!");
+                } else if (disposableCups == 0) {
+                    System.out.print("Sorry, not enough disposable cups!");
+                }
             }
-            case 3 -> {            // cappuccino
-                water -= 200;
-                milk -= 100;
-                coffeeBeans -= 12;
-                money += 6;
-                disposableCups--;
+            case "3" -> { // cappuccino
+                if (water >= 200 && milk >= 100 && coffeeBeans >= 12 && disposableCups >= 1) {
+                    water -= 200;
+                    milk -= 100;
+                    coffeeBeans -= 12;
+                    money += 6;
+                    disposableCups--;
+                    System.out.print("I have enough resources, making you a coffee!");
+                } else if (water < 200) {
+                    System.out.print("Sorry, not enough water!");
+                } else if (milk < 100) {
+                    System.out.print("Sorry, not enough milk!");
+                } else if (coffeeBeans < 12) {
+                    System.out.print("Sorry, not enough coffee beans!");
+                } else if (disposableCups == 0) {
+                    System.out.print("Sorry, not enough disposable cups!");
+                }
             }
-            default -> System.out.println("Unknown coffee type");
+            case "back" -> {}
+            default -> System.out.print("Unknown coffee type");
         }
     }
 
     public static void handleFill(Scanner scanner) {
-        System.out.println("Write how many ml of water you want to add: ");
+        System.out.print("Write how many ml of water you want to add: ");
         water += scanner.nextInt();
 
-        System.out.println("Write how many ml of milk you want to add: ");
+        System.out.print("Write how many ml of milk you want to add: ");
         milk += scanner.nextInt();
 
-        System.out.println("Write how many grams of coffee you want to add: ");
+        System.out.print("Write how many grams of coffee you want to add: ");
         coffeeBeans += scanner.nextInt();
 
-        System.out.println("Write how many disposable cups you want to add: ");
+        System.out.print("Write how many disposable cups you want to add: ");
         disposableCups += scanner.nextInt();
     }
 
     public static void handleTake() {
-        System.out.println("I gave you $" + money);
+        System.out.print("I gave you $" + money);
         money = 0;
     }
 }
--- Coffee Machine Simulator with Java/task/src/machine/README.md
+++ Coffee Machine Simulator with Java/task/src/machine/README.md
@@ -1,6 +1,14 @@
 ## Progress Log
+
+### First week
 - 2025-05-07 | 1h00 | Coffee Machine 1 & 2
 - 2025-05-10 | 1h45 | Coffee Machine 3
 - 2025-05-11 | 1h30 | Coffee Machine 4
  
 First week: 4h15
+
+### Second week
+- 2025-05-13 | 45min | Coffee Machine 5 
+- 2025-05-14 | 40min | Coffee Machine 5 + início 6
+
+Second week: 1h25
